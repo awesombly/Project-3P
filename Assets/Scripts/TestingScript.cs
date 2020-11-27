583815fad8e45b71e436f2f50c1c930c31548922
TaehongKim <walckm11@gmail.com>
2020-11-28 00:36:14 +0900

    [맵] 재질별로 발소리 재생되도록 설정

---
+++ Assets/Scripts/TestingScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingScript : MonoBehaviour
+{
+    public GameObject testLight;
+
+    void Start()
+    {
+        if ( testLight != null )
+        {
+            testLight.SetActive( false );
+        }
+    }
+}
