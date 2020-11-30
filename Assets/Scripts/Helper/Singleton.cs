f835468bfcbdad800d37561b01e21125522080c9
TaehongKim <walckm11@gmail.com>
2020-11-30 20:24:59 +0900

    [클래스 생성] c# 싱글턴 클래스 추가

---
+++ Assets/Scripts/Helper/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Singleton<Type> : MonoBehaviour where Type : MonoBehaviour
+{
+    private static Type instance;
+
+    public static Type Instance
+    {
+        get
+        {
+            if ( ReferenceEquals( instance, null ) )
+            {
+                instance = ( Type )FindObjectOfType( typeof( Type ) );
+
+                if ( ReferenceEquals( instance, null ) )
+                {
+                    GameObject singleton_object = new GameObject();
+                    instance = singleton_object.AddComponent<Type>();
+                    singleton_object.name = typeof( Type ).ToString() + " (Singleton)";
+
+                    DontDestroyOnLoad( singleton_object );
+                }
+            }
+            return instance;
+        }
+    }
+}
