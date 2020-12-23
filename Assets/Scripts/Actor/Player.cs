b23919fa571722d7a043478e714733ba5d59012f
TaehongKim <walckm11@gmail.com>
2020-12-23 11:49:37 +0900

    [캐릭터] 플레이어 Hand Bone 추가

--- Assets/Scripts/Actor/Player.cs
+++ Assets/Scripts/Actor/Player.cs
@@ -7,6 +7,10 @@ public class Player : Character
     [System.Serializable]
     public struct BoneInfo
     {
+        // 자식 추가시 원본 크기 설정용
+        static public Vector3 OriginalScale;
+
+        public Transform Reference;
         public Transform LeftHand;
         public Transform RightHand;
     }
@@ -14,18 +18,25 @@ public class Player : Character
 
     public GameObject testPrefab;
 
+    protected override void Awake()
+    {
+        base.Awake();
+        {
+            Vector3 scale = boneInfo.Reference.lossyScale;
+            BoneInfo.OriginalScale = new Vector3( 1.0f / scale.x, 1.0f / scale.y, 1.0f / scale.z );
+        }
+    }
+
     private void Start()
     {
         {
-            GameObject instance = Instantiate( testPrefab );
-            instance.transform.SetParent( boneInfo.LeftHand );
-            instance.transform.localPosition = Vector3.zero;
+            GameObject instance = Instantiate( testPrefab, boneInfo.LeftHand );
+            instance.transform.localScale = BoneInfo.OriginalScale;
         }
 
         {
-            GameObject instance = Instantiate( testPrefab );
-            instance.transform.SetParent( boneInfo.RightHand );
-            instance.transform.localPosition = Vector3.zero;
+            GameObject instance = Instantiate( testPrefab, boneInfo.RightHand );
+            instance.transform.localScale = BoneInfo.OriginalScale;
         }
     }
 }
