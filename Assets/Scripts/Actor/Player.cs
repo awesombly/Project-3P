0701df9b320f2ba94fd677e88f5bbd31bd37730a
TaehongKim <walckm11@gmail.com>
2020-12-28 17:59:29 +0900

    [캐릭터] Player 스크립트 Bone 구조 개선

--- Assets/Scripts/Actor/Player.cs
+++ Assets/Scripts/Actor/Player.cs
@@ -2,6 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum EBoneType
+{
+    Root,
+    LeftHand,
+    RightHand,
+}
+
 public class Player : Character
 {
     [System.Serializable]
@@ -10,32 +17,36 @@ public class Player : Character
         // 자식 추가시 원본 크기 설정용
         static public Vector3 OriginalScale;
 
+        public EBoneType BoneType;
         public Transform Reference;
-        public Transform LeftHand;
-        public Transform RightHand;
     }
-    public BoneInfo boneInfo;
+    public List<BoneInfo> boneList; // 인스펙터 편집용
+    private Dictionary<EBoneType, BoneInfo> boneInfos;
 
     public GameObject testPrefab;
 
     protected override void Awake()
     {
         base.Awake();
+
+        foreach ( BoneInfo info in boneList )
         {
-            Vector3 scale = boneInfo.Reference.lossyScale;
-            BoneInfo.OriginalScale = new Vector3( 1.0f / scale.x, 1.0f / scale.y, 1.0f / scale.z );
+            boneInfos.Add( info.BoneType, info );
         }
+
+        Vector3 rootScale = boneInfos[ EBoneType.Root ].Reference.lossyScale;
+        BoneInfo.OriginalScale = new Vector3( 1.0f / rootScale.x, 1.0f / rootScale.y, 1.0f / rootScale.z );
     }
 
     private void Start()
     {
         {
-            GameObject instance = Instantiate( testPrefab, boneInfo.LeftHand );
+            GameObject instance = Instantiate( testPrefab, boneInfos[ EBoneType.LeftHand ].Reference );
             instance.transform.localScale = BoneInfo.OriginalScale;
         }
 
         {
-            GameObject instance = Instantiate( testPrefab, boneInfo.RightHand );
+            GameObject instance = Instantiate( testPrefab, boneInfos[ EBoneType.RightHand ].Reference );
             instance.transform.localScale = BoneInfo.OriginalScale;
         }
     }
