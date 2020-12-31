00380e06761b694d31710de8ff3c227c39527db5
TaehongKim <walckm11@gmail.com>
2020-12-31 13:20:58 +0900

    [캐릭터/UI] 장비 퀵슬롯 슬롯 배치 작업

--- Assets/Scripts/UI/EquipQuickslot.cs
+++ Assets/Scripts/UI/EquipQuickslot.cs
@@ -7,9 +7,23 @@ public class EquipQuickslot : MonoBehaviour
     public RectTransform pannelRect;
     public float pannelRatioAtScreen;
 
-    public GameObject slotPrefab;
+    [System.Serializable]
+    public struct SlotInfo
+    {
+        public GameObject Prefab;
+        public int VerticalCount;
+        public int HorizontalCount;
+    }
+    public SlotInfo slotInfo;
+    private List<RectTransform> slotList = new List<RectTransform>();
 
     private void Start()
+    {
+        UpdatePannelTransform();
+        UpdateSlotTransform();
+    }
+
+    private void UpdatePannelTransform()
     {
         // 패널 크기를 정사각형으로 설정(짧은 축 기준)
         Vector2 anchorMin, anchorMax;
@@ -25,11 +39,31 @@ public class EquipQuickslot : MonoBehaviour
             anchorMin.x = 0.0f;
             anchorMin.y = 0.0f;
             anchorMax.x = ( pannelRatioAtScreen * Screen.height ) / Screen.width;
-            anchorMax.y = pannelRatioAtScreen; 
+            anchorMax.y = pannelRatioAtScreen;
         }
 
         pannelRect.anchorMin = anchorMin;
         pannelRect.anchorMax = anchorMax;
         pannelRect.localPosition = Vector3.zero;
+    }
+
+    private void UpdateSlotTransform()
+    {
+        int totalCount = slotInfo.VerticalCount * slotInfo.HorizontalCount;
+        for ( int i = 0; i < totalCount; ++i )
+        {
+            if ( slotList.Count <= i )
+            {
+                GameObject slot = Instantiate( slotInfo.Prefab, pannelRect );
+                slotList.Add( slot.GetComponent<RectTransform>() );
+            }
+
+            RectTransform slotRect = slotList[ i ];
+            int vIndex = ( i % slotInfo.VerticalCount );
+            int hIndex = ( i / slotInfo.VerticalCount );
+
+            slotRect.anchorMin = new Vector2( vIndex / ( float )slotInfo.VerticalCount, hIndex / ( float )slotInfo.HorizontalCount );
+            slotRect.anchorMax = new Vector2( ( vIndex + 1 ) / ( float )slotInfo.VerticalCount, ( hIndex + 1 ) / ( float )slotInfo.HorizontalCount );
+        }
     }
 }
