650e3bfdcaec42972324cac48c9c287534fd071b
wnsghks <ashi0@naver.com>
2021-01-05 00:09:29 +0900

    [AI] FSM 기본 설계

---
+++ Assets/Scripts/Character/Artificial Intelligence/AIBase.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AIBase : MonoBehaviour
+{
+    protected enum AIState : byte
+    {
+        /* 기본 상태 */
+        Idle = 0,
+
+        /* 추가 상태 */
+        Attack,
+        Move,
+        Dead,
+    };
+
+    protected AIState state { get; set; } = AIState.Idle;
+
+    protected virtual void Start()
+    {
+        StartCoroutine( ChangeState( state ) );
+    }
+
+    protected IEnumerator ChangeState( AIState _state )
+    {
+        StopCoroutine( state.ToString() );
+        state = _state;
+        yield return StartCoroutine( _state.ToString() );
+    }
+
+    protected abstract IEnumerator Idle();
+
+    protected virtual void OnExit()
+    {
+        StopAllCoroutines();
+    }
+}
