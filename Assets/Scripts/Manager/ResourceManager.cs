6270637b0db66342a16663817f834978aca26214
TaehongKim <walckm11@gmail.com>
2021-01-09 19:58:28 +0900

    [리소스] Equipment쪽 어드레서블 작업 ResourceManager로 이동, 제네릭화

--- Assets/Scripts/Manager/ResourceManager.cs
+++ Assets/Scripts/Manager/ResourceManager.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.AddressableAssets.ResourceLocators;
@@ -9,16 +8,7 @@ using UnityEngine.ResourceManagement.ResourceLocations;
 public class ResourceManager : Singleton<ResourceManager>
 {
     private Dictionary<string/*key*/, string/*guid*/> guids = new Dictionary<string/*key*/, string/*guid*/>();
-
-    public string GetAssetGuid( string _addressableKey )
-    {
-        if ( !guids.TryGetValue( _addressableKey, out string guid ) )
-        {
-            Debug.LogError( "Guid not found. key = " + _addressableKey );
-        }
-
-        return guid;
-    }
+    private Dictionary<string/*guid*/, Object/*asset*/> loadedAssets = new Dictionary<string/*guid*/, Object/*asset*/>();
 
     public void Init()
     {
@@ -32,8 +22,35 @@ public class ResourceManager : Singleton<ResourceManager>
 
             InitGuidsData();
 
-            Equipment.LoadAddressables();
+            /// TODO : Label 스크립터블로 멤버 추가
+            LoadAddressables<Equipment>( "Equipment" );
         };
+    }
+
+    public Type GetAsset<Type>( string _guid ) where Type : Object
+    {
+        if ( !loadedAssets.TryGetValue( _guid, out Object asset ) )
+        {
+            Debug.LogError( "Asset not found. Type = " + typeof( Type ).Name + ", Guid = " + _guid );
+            return null;
+        }
+
+        return asset as Type;
+    }
+
+    public Type GetAsset<Type>( AssetReference _reference ) where Type : Object
+    {
+        return GetAsset<Type>( _reference.AssetGUID );
+    }
+
+    private string GetAssetGuid( string _addressableKey )
+    {
+        if ( !guids.TryGetValue( _addressableKey, out string guid ) )
+        {
+            Debug.LogError( "Guid not found. key = " + _addressableKey );
+        }
+
+        return guid;
     }
 
     private void InitGuidsData()
@@ -64,4 +81,39 @@ public class ResourceManager : Singleton<ResourceManager>
             }
         }
     }
+
+    private void LoadAddressables<Type>( string _label ) where Type : Object
+    {
+        Addressables.LoadResourceLocationsAsync( _label ).Completed += ( locHandle ) =>
+        {
+            if ( locHandle.Status != AsyncOperationStatus.Succeeded )
+            {
+                Debug.LogError( "Failed LoadResource. label = " + _label );
+                return;
+            }
+
+            IList<IResourceLocation> locations = locHandle.Result;
+            foreach ( IResourceLocation loc in locations )
+            {
+                Addressables.LoadAssetAsync<Type>( loc ).Completed += ( assetHandle ) =>
+                {
+                    if ( assetHandle.Status != AsyncOperationStatus.Succeeded )
+                    {
+                        Debug.LogError( "Failed LoadAsset. location = " + loc.ToString() );
+                        return;
+                    }
+
+                    string guid = GetAssetGuid( loc.PrimaryKey );
+                    /// TODO : Type별로 컨테이너 분류
+                    loadedAssets.Add( guid, assetHandle.Result );
+                };
+            }
+        };
+    }
+}
+
+[System.Serializable]
+public class AssetReferenceEquipment : AssetReferenceT<Equipment>
+{
+    public AssetReferenceEquipment( string _guid ) : base( _guid ) { }
 }
--- Assets/Scripts/ScriptableObject/Equipment.cs
+++ Assets/Scripts/ScriptableObject/Equipment.cs
@@ -1,9 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement.AsyncOperations;
-using UnityEngine.ResourceManagement.ResourceLocations;
 
 public class Item : ScriptableObject
 {
@@ -41,58 +38,4 @@ public class Equipment : Item
     {
         itemType = EItemType.Equipment;
     }
-
-    private static Dictionary<string/*Guid*/ , Equipment> loadedEquipments = new Dictionary<string/*Guid*/, Equipment>();
-
-    public static Equipment GetEquipment( string _guid )
-    {
-        if ( !loadedEquipments.TryGetValue( _guid, out Equipment equip ) )
-        {
-            Debug.LogError( "Equipment not found. Guid = " + _guid );
-            return null;
-        }
-
-        return equip;
-    }
-
-    public static Equipment GetEquipment( Reference _reference )
-    {
-        return GetEquipment( _reference.AssetGUID );
-    }
-
-    #region Addressable
-    [System.Serializable]
-    public class Reference : AssetReferenceT<Equipment>
-    {
-        public Reference( string _guid ) : base( _guid ) { }
-    }
-
-    public static void LoadAddressables()
-    {
-        Addressables.LoadResourceLocationsAsync( "Equipment" ).Completed += ( locHandle ) =>
-        {
-            if ( locHandle.Status != AsyncOperationStatus.Succeeded )
-            {
-                Debug.LogError( "Failed LoadResource. label = Equipment" );
-                return;
-            }
-
-            IList<IResourceLocation> locations = locHandle.Result;
-            foreach ( IResourceLocation loc in locations )
-            {
-                Addressables.LoadAssetAsync<Equipment>( loc ).Completed += ( equipHandle ) =>
-                {
-                    if ( equipHandle.Status != AsyncOperationStatus.Succeeded )
-                    {
-                        Debug.LogError( "Failed LoadAsset. location = " + loc.ToString() );
-                        return;
-                    }
-
-                    string guid = ResourceManager.Instance.GetAssetGuid( loc.PrimaryKey );
-                    loadedEquipments.Add( guid, equipHandle.Result );
-                };
-            }
-        };
-    }
-    #endregion
 }
